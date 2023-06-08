bc03d858d8e5f3cae72980e179f8accda71baf21
yosefstar <129289620+yosefstar@users.noreply.github.com>
2023-06-08 23:48:08 +0900

    マイグレート変更

--- database/migrations/2023_06_08_072425_create_have_list_table.php
+++ database/migrations/2023_06_08_072425_create_have_list_table.php
@@ -18,8 +18,12 @@ return new class extends Migration
             $table->integer('itemPrice')->nullable();
             $table->string('itemUrl')->nullable();
             $table->string('itemCode')->unique();
+            $table->unsignedBigInteger('user_id'); // 追加されたカラム
             $table->timestamps();
             $table->string('have_status')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // 外部キー制約
+            $table->foreign('itemCode')->references('itemCode')->on('item_list')->onDelete('cascade'); // 外部キー制約
         });
     }
 
