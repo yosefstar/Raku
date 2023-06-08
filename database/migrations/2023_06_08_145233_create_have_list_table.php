a9067176e6816b54ed55aa82251854478a48aba8
yosefstar <129289620+yosefstar@users.noreply.github.com>
2023-06-08 23:53:20 +0900

    db変更

--- database/migrations/2023_06_08_145233_create_have_list_table.php
+++ database/migrations/2023_06_08_145233_create_have_list_table.php
@@ -4,12 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateHaveListTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('have_list', function (Blueprint $table) {
             $table->increments('id');
@@ -29,9 +31,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('have_list');
     }
-};
+}
